53fb1b305840b8ed3f8cca6a1d6b17491a716d64
James Zeisweiss <zeisweiss.james@gmail.com>
2018-05-07 20:26:52 -0400

    [JZ] Updating all dependencies

--- src/main/java/com/bdh/automation/AllureCommand.java
+++ src/main/java/com/bdh/automation/AllureCommand.java
@@ -1,121 +1,113 @@
 package com.bdh.automation;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.SystemUtils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.*;
+
 /**
  * Commands for creating, generating, and cleaning Allure reports.
- * 
- * @author Jimmy Zeisweiss.
  *
+ * @author Jimmy Zeisweiss.
  */
 public enum AllureCommand {
 
-	/**
-	 * Generate an Allure report using existing Allure result data files.
-	 */
-	GENERATE("generate"),
+    /**
+     * Generate an Allure report using existing Allure result data files.
+     */
+    GENERATE("generate -c"),
 
-	/**
-	 * Open an existing Allure report.
-	 */
-	OPEN("report open"),
+    /**
+     * Open an existing Allure report.
+     */
+    OPEN("open"),
 
-	/**
-	 * Remove an existing Allure report.
-	 */
-	REMOVE_OLD_REPORT("report clean");
+    /**
+     * Remove an existing Allure report.
+     */
+    REMOVE_OLD_REPORT("report clean");
 
-	private static final String CONSOLE_HORIZONTAL_LINE = "----------------------------------------------------------------------------------";
-	private static final String REPORT_DATA_PATH = "target/allure-results";
-	private static final String ALLURE_HOME = SystemUtils.USER_DIR + "/allure-tools/1.4.22";
-	private static final String TOOL_PATH = ALLURE_HOME + "/bin/allure" + (SystemUtils.IS_OS_WINDOWS ? ".bat" : "");
-	private String command;
+    private static final String CONSOLE_HORIZONTAL_LINE = "----------------------------------------------------------------------------------";
+    private static final String REPORT_DATA_PATH = "target/allure-results";
+    private static final String ALLURE_HOME = SystemUtils.USER_DIR + "/allure-tools/2.6.0";
+    private static final String TOOL_PATH = ALLURE_HOME + "/bin/allure" + (SystemUtils.IS_OS_WINDOWS ? ".bat" : "");
+    private String command;
 
-	AllureCommand(String command) {
-		this.command = command;
-	}
+    AllureCommand(String command) {
+        this.command = command;
+    }
 
-	/**
-	 * Execute a command line command without an appended value.
-	 */
-	private void executeCommand() {
-		executeCommand("");
-	}
+    /**
+     * Execute a command line command without an appended value.
+     */
+    private void executeCommand() {
+        executeCommand("");
+    }
 
-	/**
-	 * Execute a command line command with an appended value.
-	 * 
-	 * @param appendValue
-	 *            to add to the command
-	 */
-	private void executeCommand(final String appendValue) {
-		this.command = CommandLine.parse(TOOL_PATH).getExecutable() + " " + this.command + " " + appendValue;
-		System.out.println(CONSOLE_HORIZONTAL_LINE);
-		System.out.println("Running Command: $ " + command);
-		ExecutorService service = Executors.newSingleThreadExecutor();
-		Future<String> future = service.submit(new Callable<String>() {
-			@Override
-			public String call() {
-				try {
-					Process process = Runtime.getRuntime().exec(command);
-					InputStreamReader stream = new InputStreamReader(process.getInputStream());
-					BufferedReader output = new BufferedReader(stream);
-					while (process.isAlive()) {
-						String line = output.readLine();
-						if (line != null) {
-							System.out.println("OUTPUT: " + line);
-						}
-					}
-				} catch (IOException | SecurityException e) {
-					e.printStackTrace();
-				}
-				return "Success";
-			}
-		});
-		try {
-			future.get(30, TimeUnit.SECONDS);
-		} catch (TimeoutException | InterruptedException | ExecutionException e) {
-			System.out.println("Timeout reached");
-		}
-		service.shutdownNow();
-		System.out.println(CONSOLE_HORIZONTAL_LINE);
-	}
+    /**
+     * Execute a command line command with an appended value.
+     *
+     * @param appendValue to add to the command
+     */
+    private void executeCommand(final String appendValue) {
+        this.command = CommandLine.parse(TOOL_PATH).getExecutable() + " " + this.command + " " + appendValue;
+        System.out.println(CONSOLE_HORIZONTAL_LINE);
+        System.out.println("Running Command: $ " + command);
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        Future<String> future = service.submit(new Callable<String>() {
+            @Override
+            public String call() {
+                try {
+                    Process process = Runtime.getRuntime().exec(command);
+                    InputStreamReader stream = new InputStreamReader(process.getInputStream());
+                    BufferedReader output = new BufferedReader(stream);
+                    while (process.isAlive()) {
+                        String line = output.readLine();
+                        if (line != null) {
+                            System.out.println("OUTPUT: " + line);
+                        }
+                    }
+                } catch (IOException | SecurityException e) {
+                    e.printStackTrace();
+                }
+                return "Success";
+            }
+        });
+        try {
+            future.get(30, TimeUnit.SECONDS);
+        } catch (TimeoutException | InterruptedException | ExecutionException e) {
+            System.out.println("Timeout reached");
+        }
+        service.shutdownNow();
+        System.out.println(CONSOLE_HORIZONTAL_LINE);
+    }
 
-	/**
-	 * Run the current command.
-	 */
-	public void run() {
-		switch (this) {
-			case GENERATE:
-				executeCommand(REPORT_DATA_PATH);
-				break;
-			case OPEN:
-				executeCommand();
-				break;
-			case REMOVE_OLD_REPORT:
-				executeCommand();
-				FileUtils.deleteQuietly(new File(REPORT_DATA_PATH));
-				try {
-					FileUtils.forceMkdir(new File(REPORT_DATA_PATH));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				break;
-		}
-	}
+    /**
+     * Run the current command.
+     */
+    public void run() {
+        switch (this) {
+            case GENERATE:
+                executeCommand(REPORT_DATA_PATH);
+                break;
+            case OPEN:
+                executeCommand();
+                break;
+            case REMOVE_OLD_REPORT:
+                executeCommand();
+                FileUtils.deleteQuietly(new File(REPORT_DATA_PATH));
+                try {
+                    FileUtils.forceMkdir(new File(REPORT_DATA_PATH));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
 
 }
--- src/test/java/com/bdh/automation/ExampleTest.java
+++ src/test/java/com/bdh/automation/ExampleTest.java
@@ -4,15 +4,13 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-
 import ru.yandex.qatools.allure.annotations.Features;
 import ru.yandex.qatools.allure.annotations.Stories;
 
 /**
  * Example test methods showing how to use Allure.
- * 
- * @author Jimmy Zeisweiss
  *
+ * @author Jimmy Zeisweiss
  */
 @Features("Jimmy's feature")
 public class ExampleTest {
@@ -20,60 +18,58 @@ public class ExampleTest {
     private static final String BASE_URL = "https://www.google.com";
     private CoreWebDiver driver;
 
-	/**
-	 * Start the driver and clean old Allure reports.
-	 */
-	@BeforeSuite
-	public void beforeSuite() {
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-		driver = new CoreWebDiver(BASE_URL);
-        AllureCommand.REMOVE_OLD_REPORT.run();
-	}
+    /**
+     * Start the driver and clean old Allure reports.
+     */
+    @BeforeSuite
+    public void beforeSuite() {
+        // TODO: Make this Windows friendly
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        driver = new CoreWebDiver(BASE_URL);
+    }
 
-	/**
-	 * Reset the step count.
-	 */
-	@BeforeMethod
-	public void beforeMethod() {
-		driver.restStepCount();
-	}
+    /**
+     * Reset the step count.
+     */
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.restStepCount();
+    }
 
-	/**
-	 * Shutdown the driver, generate the report, and open the report.
-	 */
-	@AfterSuite
-	public void afterSuite() {
-		driver.quit();
-		AllureCommand.GENERATE.run();
-		AllureCommand.OPEN.run();
-	}
+    /**
+     * Shutdown the driver, generate the report, and open the report.
+     */
+    @AfterSuite
+    public void afterSuite() {
+        driver.quit();
+        AllureCommand.GENERATE.run();
+        AllureCommand.OPEN.run();
+    }
 
-	@Stories("Jimmy's Individual Examples")
-	@Test(description = "Demonstrate how to use steps.")
-	public void testStepExample() {
-		driver.addStep("Conquer the world")
-		      .addStep("Have fun")
-		      .addStep("Go Crazy");
-	}
+    @Stories("Jimmy's Individual Examples")
+    @Test(description = "Demonstrate how to use steps.")
+    public void testStepExample() {
+        driver.addStep("Conquer the world").addStep("Have fun").addStep("Go Crazy");
+    }
 
-	@Stories("Jimmy's Individual Examples")
-	@Test(description = "Demonstrate how to take screenshots.")
-	public void testScreenshotExample() {
-		driver.recordScreenshot("During Test example.");
-	}
+    @Stories("Jimmy's Individual Examples")
+    @Test(description = "Demonstrate how to take screenshots.")
+    public void testScreenshotExample() {
+        driver.recordScreenshot("During Test example.");
+    }
 
-	@Stories("Jimmy's Individual Examples")
-	@Test(description = "Demonstrate how to attach a data file.")
-	public void testTextAttachmentExample() {
-		driver.recordDataFile("During test data file", "{ Lots of Data }");
-	}
+    @Stories("Jimmy's Individual Examples")
+    @Test(description = "Demonstrate how to attach a data file.")
+    public void testTextAttachmentExample() {
+        driver.recordDataFile("During test data file", "{ Lots of Data }");
+    }
 
-	@Stories("Jimmy's Full Examples")
-	@Test(description = "Combine all examples.")
-	public void testFullExample() {
-		driver.addStep("Produce amazing things")
-		      .recordScreenshot("During Test example.")
-		      .recordDataFile("During test data file", "{ More Data }");
-	}
+    @Stories("Jimmy's Full Examples")
+    @Test(description = "Combine all examples.")
+    public void testFullExample() {
+        driver.addStep("Produce amazing things")
+              .recordScreenshot("During Test example.")
+              .recordDataFile("During test data file", "{ More Data }");
+    }
 
 }
